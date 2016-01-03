8fef74be2a3585dbd221ade09b2e329afc2ab0a4
Daniel Adler <weston.adler@gmail.com>
2016-01-03 13:18:52 -0800

    add calendar helper method and css from Railscast #213 for calendar view

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -2,6 +2,12 @@ class WeeksController < ApplicationController
 
   def index
     @weeks = Week.all
+
+    # Modified from Railscast #213
+    @recipes = Recipe.all
+    @recipes_by_date = @recipes.group_by(&:day_id)
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    # raise
   end
 
 end
